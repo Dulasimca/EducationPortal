83f7b9727c773a879df876fff970499fd3146bdd
srikanth2419 <srikanthraj.raj@gmail.com>
2021-09-02 12:04:02 +0530

    circular added

--- src/app/Forms-Module/circular-form/circular-form.component.ts
+++ src/app/Forms-Module/circular-form/circular-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import {NgForm} from '@angular/forms';
+
 
 @Component({
   selector: 'app-circular-form',
@@ -9,21 +12,48 @@ export class CircularFormComponent implements OnInit {
   Subject: string;
   Instructions:string;
   
+  school_id:string;
   date: Date = new Date();
   data: any = [];
 
-  uploadedFiles: any[] = [];
+  _guardianimg: any[] = [];
   
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
   onFileUpload($event, id) {
     // for(let file of event.files) {
     //     this.uploadedFiles.push(file);
-    // }
+     //}
 
    // this.messageService.add({severity: 'info', summary: 'File Uploaded', detail: ''});
+}
+
+
+onSave(form: NgForm) { 
+  //alert("hi")
+  //alert(this.date)
+   
+  var detail = {
+   'subject': (this.Subject !==undefined && this.Subject !==null) ? this.Subject : "",
+   'instruction': (this.Instructions !==undefined && this.Instructions !==null) ? this.Instructions : "",
+   'date': (this.date !==undefined && this.date !==null) ? this.date : "",
+   'upload': (this._guardianimg !==undefined && this._guardianimg !==null) ? this._guardianimg : "",
+   'school_id': (this.school_id !==undefined && this.school_id !==null) ? this.school_id : ""
+  
+  }
+  //alert(detail);
+  console.log(detail);
+ /* this.http.post('http://localhost:22100/api/employee/post', detail).subscribe(response => {
+    if(response) {
+      alert("saved successfully");
+          form.resetForm();
+     this.onview();
+    }
+
+  })*/
+
 }
 
 }

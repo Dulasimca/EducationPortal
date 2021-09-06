f00103ae7cc19f71641a79e7757b8004560bd276
srikanth2419 <srikanthraj.raj@gmail.com>
2021-09-06 12:24:14 +0530

    Changes added

--- src/app/Common-Module/PathConstants.ts
+++ src/app/Common-Module/PathConstants.ts
@@ -1,4 +1,8 @@
 export class PathConstants {
     public static readonly Registration_Post = 'RegistrationForm/Post';
     public static readonly Circular_Post = 'Circular/Post';
+    public static readonly NewsLetter_Post = 'NewsLetter/post';
+    public static readonly Announcement_Post = 'Announcement/Post';
+    public static readonly Assignment_Post = 'Assignments/Post';
+    
 }

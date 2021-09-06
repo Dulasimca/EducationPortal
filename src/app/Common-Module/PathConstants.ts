2caba804810c38fcb51fdf2e286e84aa58c7d348
sivamanimohan <sivamanimohan@outlook.com>
2021-09-06 12:10:56 +0530

    bookholidaymyachievement added

--- src/app/Common-Module/PathConstants.ts
+++ src/app/Common-Module/PathConstants.ts
@@ -1,3 +1,7 @@
 export class PathConstants {
     public static readonly Registration_Post = 'RegistrationForm/Post';
+    public static readonly Book_Post = 'Books/Post';
+    public static readonly Holiday_Post = 'Holidays/Post';
+    public static readonly Myachievement_Post = 'MyAchievements/Post';
+    
 }

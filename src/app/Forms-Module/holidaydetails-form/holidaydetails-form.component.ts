1fb1e41e70d8311fae7de3fc3f6ba8b293c011ce
Adithya0802 <adithya08feb1997@gmail.com>
2021-09-14 17:53:07 +0530

    forms added

--- src/app/Forms-Module/holidaydetails-form/holidaydetails-form.component.ts
+++ src/app/Forms-Module/holidaydetails-form/holidaydetails-form.component.ts
@@ -2,10 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { SelectItem } from 'primeng/api';
 import { PathConstants } from 'src/app/Common-Module/PathConstants';
 import { RestAPIService } from 'src/app/Services/restAPI.service';
-import { saveAs } from 'file-saver';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { MatNativeDateModule } from '@angular/material/core';
+import { DatePipe } from '@angular/common';
 
 
 
@@ -25,7 +23,7 @@ export class HolidaydetailsFormComponent implements OnInit {
   data: any = []; 
   cols: any;
   MRowid=0;
-  constructor(private restApiService: RestAPIService, private http: HttpClient) { }
+  constructor(private restApiService: RestAPIService, private http: HttpClient,private datepipe: DatePipe) { }
 
   ngOnInit(): void {
     this.typeOptions = [
@@ -54,7 +52,7 @@ export class HolidaydetailsFormComponent implements OnInit {
       'SchoolId': 1,
       'EventDetailS':this.Events,
       'Holiday': this.selectedType,     
-      'eventdate': this.date, 
+      'eventdate': this.datepipe.transform(this.date,'yyyy-MM-dd'), 
       'Flag': 1,      
     };
     this.restApiService.post(PathConstants.Holiday_Post, params).subscribe(res => {

f1baa99250cb4d7b592181efa8bb57285839457e
srikanth2419 <srikanthraj.raj@gmail.com>
2021-09-21 10:59:10 +0530

    newsletter Book Circular Added gallery result

--- src/app/Forms-Module/downloadsession-form/downloadsession-form.component.ts
+++ src/app/Forms-Module/downloadsession-form/downloadsession-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-downloadsession-form',
@@ -11,12 +12,25 @@ export class DownloadsessionFormComponent implements OnInit {
   Duration:string;
   date: Date = new Date();
   data: any = [];
-
+  uploadedFiles: any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
     
   }
+  onFileUpload($event, id) {
+    console.log('eve', $event);
+    const reader = new FileReader();
+    var selectedFile = $event.target.files[0];
+    console.log('file', selectedFile);
+   reader.readAsDataURL(selectedFile);
+   console.log('url', reader.readAsDataURL(selectedFile));
+    // var endpoint = '../../assets/layout/circular_image';
+    // this.http.post(endpoint, selectedFile).subscribe
+    (res => 
+    {
 
+   })
+  }
 }

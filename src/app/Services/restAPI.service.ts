98d525366969f67b610e344c345ca67df342072a
chandrika <udhayachandrikam31@gmail.com>
2021-11-10 14:37:44 +0530

    remember me changes

--- src/app/Services/restAPI.service.ts
+++ src/app/Services/restAPI.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { delay, timeout, retry, catchError } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 
 
 @Injectable({
